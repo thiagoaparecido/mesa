1272a2b0d09c2841e230c03fda4c104a27c3c403
Thiago Santos <thiago_aparecido@hotmail.com>
2018-04-04 16:07:14 -0300

    Add files via upload

---
+++ ClientApp/app/models/perfil/perfil-endereco-correspondencia.ts
@@ -0,0 +1,11 @@
+export class PerfilEnderecoCorrespondencia {
+    idCliente: number;
+    sqEndereco: number;
+    logradouro: string;
+    numero: string;
+    complemento: string;
+    bairro: string;
+    cidade: string;
+    idEstado: string;
+    cep: string;
+}
